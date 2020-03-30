778e2685f861a968a67e603303cd2547239264ac
Nguyễn Sỹ Tùng <tungaqhd@gmail.com>
2020-03-30 17:09:08 +0700

    demo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
     res.send('hello there');
-}))
+});
 router.use('/webhook', require('./webhook'));
 router.use('/admin', require('./admin'));
 
