9a14486886925fd97355c462c4d7fa5a8c72afc0
Nguyễn Sỹ Tùng <tungaqhd@gmail.com>
2020-03-30 17:31:44 +0700

    demo

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const bot = require('../helpers/bot');
-router.post('/', async(req, res) => {
+router.post('/', async (req, res) => {
     try {
-        const message = req.body.message.text;
-        const senderId = req.body.sender.id;
-        
-        await bot.sendMessage(senderId, `Bạn vừa nhắn ${message}`);
+        if (req.body.event_name == "user_send_text") {
+            const message = req.body.message.text;
+            const senderId = req.body.sender.id;
+
+            await bot.sendMessage(senderId, `Bạn vừa nhắn ${message}`);
+        }
         res.send();
-    } catch(e) {
+    } catch (e) {
         res.status(500);
         console.log(e);
         res.send(e);
