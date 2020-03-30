f1316f218a644014a5163633d8b4d6b38bb3f450
Nguyễn Sỹ Tùng <tungaqhd@gmail.com>
2020-03-30 17:20:05 +0700

    demo

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-exports.PORT = process.env.port || 3000
+exports.PORT = process.env.PORT || 3000
 exports.ACCESS_TOKEN = "aZtR1X6Dao6METG64egN8Pu-r0aRbC91_5hZ9p6QrmF7UvudIgoF1Oj1ktnqhvecccZQRsc_nYQh2F5oTkMpEBeeynj_nU52WpBPEmF-a67S4w4y3U6sPjGL_bilsiDvpYdo1pRywtgs6zmrGVJy4PORkqjXgBerxHYFT1h6gGBOEh9c6uprD9i-r7PssDDIX1t80rgXodg9Tu8vHP6USx1oYnLnmvTLztxJ5IYAnaB3LvidEuI1TSLQnWS7Zeznz3ckKmB_wmvfORI4S10Lbvyb";
 
 exports.APP_SECRET = 'you app_secret';
