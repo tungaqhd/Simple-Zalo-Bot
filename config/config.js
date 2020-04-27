e9054d86e9cdf751c81f7e0f83e7d12d65f34fad
Nguyễn Sỹ Tùng <tungaqhd@gmail.com>
2020-04-27 18:58:28 +0700

    Xoa thong tin

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 exports.PORT = process.env.PORT || 3000
-exports.ACCESS_TOKEN = "7G_f4cLvIWmP1-uUPt9B01e1uHnZQ050Pa31MNbGDa5JHCzTBpGO4YiomG4aJ1rc2ptM70elCryNMvKYAYjiFniYfo45Atv-1JsgFma9VbaYDeC4MW1tR6btg3PYHrfzVLcc6WHrSmW-VAX5FMjr9H50aZSXIsHx1K77C1fUBarWKufUO2n8Baybg4jN4KqaVnwiLnihPX4NFu5VR3CYC6j3toDhANvPRGs1Cd4_Ubj_AeeuT5HxUarNXZzUUKP7N5sY1qz_V1vVQeLRD4b7aofdPaiE";
-exports.APP_ID = '1611045978726444960';
-exports.OA_SECRET_KEY = 'hCC31HBcVFTh3vu2gYnr';
+exports.ACCESS_TOKEN = "";
+exports.APP_ID = '';
+exports.OA_SECRET_KEY = '';
 
-exports.ADMIN_USERNAME = 'admin';
-exports.ADMIN_PASSWORD = 'admin-zalo-bot';
+exports.ADMIN_USERNAME = '';
+exports.ADMIN_PASSWORD = '';
 
-exports.JWT_TOKEN = 'abc@123$$##@$##$%%$%jbjbfnwjefiwejnf';
+exports.JWT_TOKEN = '';
--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = 'mongodb://tungaqhd:HiD0ihIuVi6481fR@cluster0-shard-00-00-cmjkf.mongodb.net:27017,cluster0-shard-00-01-cmjkf.mongodb.net:27017,cluster0-shard-00-02-cmjkf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+const uri = '';
 mongoose.connect(uri, {
     useFindAndModify: false,
     useCreateIndex: true,
